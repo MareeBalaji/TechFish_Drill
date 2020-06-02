2b4b23be343b1e572b28d8ab2f1870e1ac2efcee
MareeBalaji <mareeswaran.balaji@gmail.com>
2020-06-02 20:17:40 +0530

    first commit

--- target/classes/results/report.js
+++ target/classes/results/report.js
@@ -96,14 +96,14 @@ formatter.match({
   "location": "TransferBetweenAccounts_Vimal.launch_the_browser_Vimal()"
 });
 formatter.result({
-  "duration": 10155833500,
+  "duration": 9348013000,
   "status": "passed"
 });
 formatter.match({
   "location": "TransferBetweenAccounts_Vimal.navigate_to_the_URL_Vimal()"
 });
 formatter.result({
-  "duration": 1360762600,
+  "duration": 5381803700,
   "status": "passed"
 });
 formatter.scenario({
@@ -157,26 +157,28 @@ formatter.match({
   "location": "TransferBetweenAccounts_Vimal.user_enters_the_credentials_and_login_to_the_application__Vimal(String)"
 });
 formatter.result({
-  "duration": 7726369400,
-  "error_message": "org.openqa.selenium.NoSuchWindowException: no such window: target window already closed\nfrom unknown error: web view not found\n  (Session info: chrome\u003d83.0.4103.61)\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027MAVBCP75\u0027, ip: \u0027192.168.43.19\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_231\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 83.0.4103.61, chrome: {chromedriverVersion: 83.0.4103.39 (ccbf011cb2d2b..., userDataDir: C:\\Users\\VIMALR~1\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:49570}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 122d496b0642f79faf8758261c49e528\n*** Element info: {Using\u003dxpath, value\u003d//input[@id\u003d\u0027username\u0027]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat com.stepdefination.TransferBetweenAccounts_Vimal.user_enters_the_credentials_and_login_to_the_application__Vimal(TransferBetweenAccounts_Vimal.java:52)\r\n\tat ✽.Given User enters the credentials and login to the application\"8\"_Vimal(feature/TransferBetweenAccounts_Vimal.feature:38)\r\n",
-  "status": "failed"
+  "duration": 14860507800,
+  "status": "passed"
 });
 formatter.match({
   "location": "TransferBetweenAccounts_Vimal.navigate_to_transfer_between_own_accounts_page_Vimal()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 3280539400,
+  "status": "passed"
 });
 formatter.match({
   "location": "TransferBetweenAccounts_Vimal.user_selects_Primary_to_Savings_transfer_and_enters_a_invalid_amount_Vimal()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 7198820400,
+  "status": "passed"
 });
 formatter.match({
   "location": "TransferBetweenAccounts_Vimal.fund_Transfer_is_should_be_rejected_Vimal()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 1140541500,
+  "status": "passed"
 });
 });
